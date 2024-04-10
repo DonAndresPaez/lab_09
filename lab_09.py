d7e4f1c2c8aec3de2b38d188d04591fd7801efb4
DonAndresPaez <andrespaez1409@gmail.com>
2024-04-10 15:06:11 -0400

    First commit with the encoder. Made by Andres

---
+++ lab_09.py
@@ -0,0 +1,24 @@
+def encode_password(password):
+    encoded_password = ""
+    for digit in password:
+        encoded_digit = str((int(digit) + 3) % 10)
+        encoded_password += encoded_digit
+    return encoded_password
+
+
+def main():
+
+    while True:
+        password = input("Enter your 8-digit password: ")
+        if len(password) != 8 or not password.isdigit():
+            print("Invalid password format. Please enter an 8-digit password containing only integers.")
+        else:
+            break
+
+    encoded_password = encode_password(password)
+
+    print("Encoded password:", encoded_password)
+
+
+if __name__ == "__main__":
+    main()
