103246e0f5ec3ad1e644fb91bc190a4bece4c86a
TJPcodes <tyler920pencinger@gmail.com>
2024-04-10 20:47:07 -0400

    This is my third commit. Added Decoder and commented out what I believed unneccesary

--- lab_09.py
+++ lab_09.py
@@ -5,22 +5,51 @@ def encode_password(password):
         encoded_password += encoded_digit
     return encoded_password
 
+def decode(encoded_password):
+    original_password = ""
+    for char in encoded_password:
+        decoded_digit = (int(char) - 3) % 10
+        original_password += str(decoded_digit)
+    return original_password
+
 
 def main():
 
     while True:
-        password = input("Enter your 8-digit password: ")
-        if len(password) != 8 or not password.isdigit():
-            print("Invalid password format. Please enter an 8-digit password containing only integers.")
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode_password(password)
+            print("Your password has been encoded and stored!")
+        elif option == '2':
+            if 'encoded_password' in locals():
+                original_password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {original_password}.")
+            else:
+                print("No encoded password stored. Please encode a password first.")
         else:
+            option == '3';
             break
+        # else:
+        #     print("Invalid option, please try again.")
+        # password = input("Enter your 8-digit password: ")
+        # if len(password) != 8 or not password.isdigit():
+        #     print("Invalid password format. Please enter an 8-digit password containing only integers.")
+        # else:
+        #     break
 
-    encoded_password = encode_password(password)
-
-    print("Encoded password:", encoded_password)
+    # encoded_password = encode_password(password)
+    #
+    # print("Encoded password:", encoded_password)
 
 
 if __name__ == "__main__":
     main()
 
-tyler joe
+
